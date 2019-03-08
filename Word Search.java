65f501c39eacf00ceecb78da1aba47b02cc897d3
riaing <panjfang@gmail.com>
2019-03-07 18:03:13 -0800

    Update Word Search.java

--- Word Search.java
+++ Word Search.java
@@ -81,98 +81,11 @@ public class Solution {
     }
 }
 
----------improve programming----------
-
-public class Solution {
-    int length; 
-    int row;
-    int col; 
-    
-    public boolean exist(char[][] board, String word) {
-        if(board == null || board.length ==0 || board[0].length == 0 ){
-            return false;
-        }
-         row = board.length;
-         col = board[0].length; 
-         
-        if(word.equals(null)){
-            return true; 
-        }
-        
-        length = word.length() ;
-        int curLoc = 0; 
-        boolean[][] visited = new boolean[row][col];
-        
-        for(int i =0 ; i< row; i ++ ){
-            for(int j =0; j<col ;j ++ ){
-                if(board[i][j] == word.charAt(0)){
-                   if(dfs(board, word, curLoc, i , j, visited)){ // 如果dfs直接找到了路径，返回T，如果走完matrix仍没，则F
-                       return true;
-                   }
-                }
-            }
-        }
-        return false; 
-    }
-    
-    private boolean dfs(char[][] board, String word, int curLoc, int x, int y, boolean[][] visited){
-        if(curLoc == length ){ //or: curLoc == length -1 得把这个放到第一位，先判断。最后一个走完进这里return
-             return true;
-        }
-        
-        //if((x <0 || x>= row || y <0 || y >= col) || visited[x][y] ==true){ //或放到后面的if里，注意顺序得放在
-                                                                            //board[x][y] ==word.charAt(curLoc) 之前
-          //  return false; 
-        //}
-        
-        if(  (x >=0 && x < row && y >=0 && y < col) && visited[x][y] ==false && board[x][y] ==word.charAt(curLoc) ){
-            visited[x][y] = true; 
-             curLoc++;
-             int[] dx = {-1, 1, 0, 0}; //避免写四个递归的方法
-	        int[] dy = {0, 0, -1, 1};
-	        for(int d =0; d<4; d ++){ //新知识，学习
-	            int i = x + dx[d];
-	            int j = y +dy[d]; 
-	                if( dfs(board, word, curLoc, i ,j, visited)){
-	                    return true;
-	                }
-	            
-	        }
-	        visited[x][y] =false; //注意要回溯！ 
-        }
-	    return false;
-	    
-        
-        之前的dfs写法： 
-       /* if(curLoc == length ){ //or: curLoc == length -1 得把这个放到第一位，先判断。最后一个走完进这里return
-             return true;
-        }   
-        if(i <0 || i>= row || j <0 || j>= col){
-            return; 
-        }
-        if(visited[i][j] == true){
-            return; 
-        }*/
- 
-      /*  if(board[i][j] ==word.charAt(curLoc)){
-            visited[i][j] =true;
-            curLoc++;
-            if(ifright == false ){ //每次递归前都要判断是否已可返回，避免重复劳动
-            dfs(board, word, curLoc, i-1, j,visited); 
-            }
-            if(ifright == false ){
-            dfs(board, word, curLoc, i+1, j,visited); 
-            }
-            if(ifright == false ){
-            dfs(board, word, curLoc, i, j-1,visited); 
-            }
-            if(ifright == false ){
-            dfs(board, word, curLoc, i, j+1, visited);
-            }
-            visited[i][j] =false; //记得要回溯
-        }*/
-         
 ----------------------------直接从这开始看---------------------------------------------
+time O(mn*4^k) where k is the length of the string; mn for for loop and for the dfs method its 4^k.Since the dfs 
+method goes only as deep as the word length we have T(k)=4(T(k-1))=4*4T(k-2)=....=.. which will be 4^k. 
+	
+	
 first backTracking solution 1: 
 	    class Solution {
     public boolean exist(char[][] board, String word) {
